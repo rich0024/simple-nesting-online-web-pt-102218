996380d8d96bf196fef9426342388972efa736cf
Richard Yance <yance0022@gmail.com>
2018-11-05 03:24:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -91,6 +91,8 @@ def adding_matz
 		   :languages => ["LISP", "C"]
 		 }
 
+		 programmer_hash
+
 end
 
 def changing_alan
@@ -111,7 +113,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      programmer_hash[:alan_kay][:known_for] = "GUI"
-
+		 programmer_hash
 
 end
 
