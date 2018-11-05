8e3373a277122e702252c774c3f74e15d53e9175
Richard Yance <yance0022@gmail.com>
2018-11-05 02:58:17 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
      }
 
 		 programmer_hash[:dennis_ritchie][:languages]
-		
+
 end
 
 def adding_matz
@@ -89,7 +89,7 @@ def adding_matz
 		 programmer_hash[:yukihiro_matsumoto] => {
 		   :known_for => "Ruby",
 		   :languages => ["LISP", "C"]
-			 
+
 end
 
 def changing_alan
@@ -109,7 +109,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 
 
 end
